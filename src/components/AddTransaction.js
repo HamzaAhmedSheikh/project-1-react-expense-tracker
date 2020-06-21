7ef7fefd8f420b13d2895ff08f800926b833ed41
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-21 22:29:29 +0500

    update AddTransaction component

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
+
+import { GlobalContext } from '../context/GlobalState'
 
 export const AddTransaction = () => {
 
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
 
+  const { addTransaction } = useContext(GlobalContext)
+
+  const onSubmit = e => {
+    e.preventDefault()
+
+   const newTransaction = {
+     id: Math.floor(Math.random() * 100000000),
+     text,
+     amount
+   } 
+
+      addTransaction(newTransaction)
+ }
+
+
     return (
       <div>
         <h3> Add new Transaction </h3> 
           
-        <form>
+        <form onSubmit={onSubmit}>
           <div className='form-control'>
             <label htmlFor='text'> Text </label>
             <input type='text'
--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,5 +1,11 @@
 export default (state, action) => {
    switch (action.type) {
+
+      case "ADD_TRANSACTION":
+         return{
+            ...state,
+            transactions: [action.payload, ...state.transactions] 
+         }
       
       case "DELETE_TRANSACTION":
         return{
