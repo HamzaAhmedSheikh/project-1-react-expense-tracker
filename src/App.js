039c88c99be73840d77d6cc10f93ceef9c748457
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-21 19:23:57 +0500

    I make AddTransaction component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Header } from './components/Header'
 import { Balance } from './components/Balance'
 import { IncomeExpenses } from './components/IncomeExpenses'
 import { TransactionList } from './components/TransactionList'
+import { AddTransaction } from './components/AddTransaction'
+
 import './App.css';
 
 function App() {
@@ -14,6 +16,7 @@ function App() {
         <Balance />
         <IncomeExpenses />
         <TransactionList />
+        <AddTransaction />
       </div>
     </div>
   );
