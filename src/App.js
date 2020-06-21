6a0e1f7c1b4ddd0244a794454ba52a8784170112
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-21 15:55:26 +0500

    I am adding Balance.js component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from './components/Header'
+import { Balance } from './components/Balance'
 
 import './App.css';
 
@@ -7,6 +8,10 @@ function App() {
   return (
     <div>
       <Header />
+
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
