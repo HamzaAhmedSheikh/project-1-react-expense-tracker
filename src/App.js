f5daef8f30ea207d32f62cf3306e4712121525c7
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-21 15:34:03 +0500

    Add CSS

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Header } from './components/Header'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Hello World Testing 123</h1>
+    <div>
+      <Header />
     </div>
   );
 }
